7ef2276b6a51bdce56f88d751846a495d7dab273
Ronald Lambino <50892397+nadlambino@users.noreply.github.com>
2024-01-09 16:30:20 +0800

    docs: docblocks

--- RequestBody.php
+++ RequestBody.php
@@ -6,8 +6,18 @@ namespace Inspira\Http;
 
 use InvalidArgumentException;
 
+/**
+ * Represents the request body, extending the abstract Body class.
+ */
 class RequestBody extends Body
 {
+	/**
+	 * Construct a new RequestBody object.
+	 *
+	 * @param mixed $stream The stream resource for the request body.
+	 *
+	 * @throws InvalidArgumentException If an invalid stream resource is provided.
+	 */
 	public function __construct(protected mixed $stream = null)
 	{
 		if (!empty($this->stream) && !is_resource($this->stream)) {
