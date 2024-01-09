e6f2bb10d6a7ea6c8dbb2d090e4c8f823ef9402d
Ronald Lambino <50892397+nadlambino@users.noreply.github.com>
2024-01-09 16:28:00 +0800

    docs: docblocks

--- Body.php
+++ Body.php
@@ -11,24 +11,16 @@ abstract class Body implements StreamInterface
 {
 	protected mixed $stream;
 
+	/**
+	 * Close the stream and the underlying resources upon class destruction.
+	 */
 	public function __destruct()
 	{
 		$this->close();
 	}
 
 	/**
-	 * Reads all data from the stream into a string, from the beginning to end.
-	 *
-	 * This method MUST attempt to seek to the beginning of the stream before
-	 * reading data and read the stream until the end is reached.
-	 *
-	 * Warning: This could attempt to load a large amount of data into memory.
-	 *
-	 * This method MUST NOT raise an exception in order to conform with PHP's
-	 * string casting operations.
-	 *
-	 * @see http://php.net/manual/en/language.oop5.magic.php#object.tostring
-	 * @return string
+	 * {@inheritdoc}
 	 */
 	public function __toString(): string
 	{
@@ -37,9 +29,7 @@ abstract class Body implements StreamInterface
 	}
 
 	/**
-	 * Closes the stream and any underlying resources.
-	 *
-	 * @return void
+	 * {@inheritdoc}
 	 */
 	public function close(): void
 	{
@@ -50,11 +40,7 @@ abstract class Body implements StreamInterface
 	}
 
 	/**
-	 * Separates any underlying resources from the stream.
-	 *
-	 * After the stream has been detached, the stream is in an unusable state.
-	 *
-	 * @return resource|null Underlying PHP stream, if any
+	 * {@inheritdoc}
 	 */
 	public function detach() : mixed
 	{
@@ -65,9 +51,7 @@ abstract class Body implements StreamInterface
 	}
 
 	/**
-	 * Get the size of the stream if known.
-	 *
-	 * @return int|null Returns the size in bytes if known, or null if unknown.
+	 * {@inheritdoc}
 	 */
 	public function getSize(): ?int
 	{
@@ -77,10 +61,7 @@ abstract class Body implements StreamInterface
 	}
 
 	/**
-	 * Returns the current position of the file read/write pointer
-	 *
-	 * @return int Position of the file pointer
-	 * @throws RuntimeException on error.
+	 * {@inheritdoc}
 	 */
 	public function tell(): int
 	{
@@ -94,9 +75,7 @@ abstract class Body implements StreamInterface
 	}
 
 	/**
-	 * Returns true if the stream is at the end of the stream.
-	 *
-	 * @return bool
+	 * {@inheritdoc}
 	 */
 	public function eof(): bool
 	{
@@ -104,9 +83,7 @@ abstract class Body implements StreamInterface
 	}
 
 	/**
-	 * Returns whether the stream is seekable.
-	 *
-	 * @return bool
+	 * {@inheritdoc}
 	 */
 	public function isSeekable(): bool
 	{
@@ -116,16 +93,7 @@ abstract class Body implements StreamInterface
 	}
 
 	/**
-	 * Seek to a position in the stream.
-	 *
-	 * @link http://www.php.net/manual/en/function.fseek.php
-	 * @param int $offset Stream offset
-	 * @param int $whence Specifies how the cursor position will be calculated
-	 *     based on the seek offset. Valid values are identical to the built-in
-	 *     PHP $whence values for `fseek()`.  SEEK_SET: Set position equal to
-	 *     offset bytes SEEK_CUR: Set position to current location plus offset
-	 *     SEEK_END: Set position to end-of-stream plus offset.
-	 * @throws RuntimeException on failure.
+	 * {@inheritdoc}
 	 */
 	public function seek(int $offset, int $whence = SEEK_SET): void
 	{
@@ -137,14 +105,7 @@ abstract class Body implements StreamInterface
 	}
 
 	/**
-	 * Seek to the beginning of the stream.
-	 *
-	 * If the stream is not seekable, this method will raise an exception;
-	 * otherwise, it will perform a seek(0).
-	 *
-	 * @throws RuntimeException on failure.
-	 * @link http://www.php.net/manual/en/function.fseek.php
-	 * @see seek()
+	 * {@inheritdoc}
 	 */
 	public function rewind(): void
 	{
@@ -152,9 +113,7 @@ abstract class Body implements StreamInterface
 	}
 
 	/**
-	 * Returns whether the stream is writable.
-	 *
-	 * @return bool
+	 * {@inheritdoc}
 	 */
 	public function isWritable(): bool
 	{
@@ -165,11 +124,7 @@ abstract class Body implements StreamInterface
 	}
 
 	/**
-	 * Write data to the stream.
-	 *
-	 * @param string $string The string that is to be written.
-	 * @return int Returns the number of bytes written to the stream.
-	 * @throws RuntimeException on failure.
+	 * {@inheritdoc}
 	 */
 	public function write(string $string): int
 	{
@@ -187,9 +142,7 @@ abstract class Body implements StreamInterface
 	}
 
 	/**
-	 * Returns whether the stream is readable.
-	 *
-	 * @return bool
+	 * {@inheritdoc}
 	 */
 	public function isReadable(): bool
 	{
@@ -200,14 +153,7 @@ abstract class Body implements StreamInterface
 	}
 
 	/**
-	 * Read data from the stream.
-	 *
-	 * @param int $length Read up to $length bytes from the object and return
-	 *     them. Fewer than $length bytes may be returned if underlying stream
-	 *     call returns fewer bytes.
-	 * @return string Returns the data read from the stream, or an empty string
-	 *     if no bytes are available.
-	 * @throws RuntimeException if an error occurs.
+	 * {@inheritdoc}
 	 */
 	public function read(int $length): string
 	{
@@ -225,11 +171,7 @@ abstract class Body implements StreamInterface
 	}
 
 	/**
-	 * Returns the remaining contents in a string
-	 *
-	 * @return string
-	 * @throws RuntimeException if unable to read or an error occurs while
-	 *     reading.
+	 * {@inheritdoc}
 	 */
 	public function getContents(): string
 	{
@@ -243,16 +185,7 @@ abstract class Body implements StreamInterface
 	}
 
 	/**
-	 * Get stream metadata as an associative array or retrieve a specific key.
-	 *
-	 * The keys returned are identical to the keys returned from PHP's
-	 * stream_get_meta_data() function.
-	 *
-	 * @link http://php.net/manual/en/function.stream-get-meta-data.php
-	 * @param string|null $key Specific metadata to retrieve.
-	 * @return array|mixed|null Returns an associative array if no key is
-	 *     provided. Returns a specific key value if a key is provided and the
-	 *     value is found, or null if the key is not found.
+	 * {@inheritdoc}
 	 */
 	public function getMetadata(?string $key = null): mixed
 	{
