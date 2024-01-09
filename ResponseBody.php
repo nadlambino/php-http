42330a6e95da0f3aad9e5082d97f5cb20c75652b
Ronald Lambino <50892397+nadlambino@users.noreply.github.com>
2024-01-09 16:30:53 +0800

    docs: docblocks

--- ResponseBody.php
+++ ResponseBody.php
@@ -6,8 +6,18 @@ namespace Inspira\Http;
 
 use InvalidArgumentException;
 
+/**
+ * Represents the response body, extending the abstract Body class.
+ */
 class ResponseBody extends Body
 {
+	/**
+	 * Construct a new ResponseBody object.
+	 *
+	 * @param mixed $stream The stream resource for the response body.
+	 *
+	 * @throws InvalidArgumentException If an invalid stream resource is provided.
+	 */
 	public function __construct(protected mixed $stream = null)
 	{
 		if (!empty($this->stream) && !is_resource($this->stream)) {
